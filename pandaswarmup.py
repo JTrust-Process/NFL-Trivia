155d903c5e0437e2f08b8f163ec49e62682e42ab
JTrust-Process <109557106+JTrust-Process@users.noreply.github.com>
2025-09-16 20:55:53 -0400

    Testing certain conditions within the code

--- pandaswarmup.py
+++ pandaswarmup.py
@@ -3,9 +3,13 @@ import pandas as pd
 # Load CSV into a DataFrame
 questions = pd.read_csv("nfl_trivia.csv")
 
+pd.set_option("display.max_columns", None)  # show all columns
+pd.set_option("display.width", None)
+
 # Look at the first few rows
 print(questions.head())
 
+print()
 # Example: filter all "Super Bowl" category questions
 super_bowl_qs = questions[questions["category"] == "Super Bowl"]
 print(super_bowl_qs)
